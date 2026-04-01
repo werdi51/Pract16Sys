d4255122f436d4b2b9143be0423e219ef6417729
werdi51 <spotifig2@gmail.com>
2026-04-01 14:57:50 +0500

    madlen
    wty\

--- Pract16Sys.ino
+++ Pract16Sys.ino
@@ -1,10 +1,54 @@
 
+#define Green 7
+#define Yellow 9 
+#define Red 10
+
+bool isConfigured = false;
+
+int input_pin = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+// настройка последовательного порта
+Serial.begin(9600);
+Serial.println("\nНастройка пина для диода. Введите номер пина:");
+
+
+while (Serial.available() == 0) {
+}
+input_pin = Serial.parseInt();
+
+if (input_pin != 0) {
+
+pinMode(Green, OUTPUT);
+pinMode(Red, OUTPUT);
+pinMode(Yellow, OUTPUT);
+
+isConfigured = true;
+} 
+
 
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
- 
+if (isConfigured) {
+
+digitalWrite(Red, LOW);
+digitalWrite(Yellow, HIGH);
+
+delay(input_pin);
+
+digitalWrite(Yellow, LOW);
+digitalWrite(Green, HIGH);
+
+delay(input_pin);
+
+digitalWrite(Green, LOW);
+digitalWrite(Red, HIGH);
+
+delay(input_pin);
+
+}
+
+
 }
